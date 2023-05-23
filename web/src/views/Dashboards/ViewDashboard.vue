cf70c93102c56b7678d0e6069a8ab69e1f5faa05
ktx-keshavi <keshavi.bhalani@kiara.tech>
2023-05-23 10:41:26 +0530

    add data-test in dashboard file

--- web/src/views/Dashboards/ViewDashboard.vue
+++ web/src/views/Dashboards/ViewDashboard.vue
@@ -23,7 +23,7 @@
         <span class="q-table__title q-mx-md q-mt-xs">{{ list[0].title }}</span>
       </div>
       <div class="flex">
-        <q-btn outline padding="xs" no-caps icon="add" @click="addPanelData">
+        <q-btn outline padding="xs" no-caps icon="add" @click="addPanelData" data-test="dashboard-panel-add" >
           <q-tooltip>{{ t('panel.add') }}</q-tooltip>
         </q-btn>
         <!-- <q-btn class="q-ml-md q-mb-xs text-bold" outline padding="sm lg" color="white" text-color="black" no-caps
